42fd7e63e7f478a4a3146636adba8bf49637d446
Roger Murariu <Roger.Murariu@endava.com>
2021-04-28 15:35:18 +0300

    Add project files.

---
+++ InceptionCodingChallenge/Dream.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InceptionCodingChallenge
+{
+    public class Dream
+    {
+        public int Ticks { get; set; }
+        public double InnerDreamTicks { get; set; }
+        public int Depth = 0;
+        public Dream InnerDream { get; set; }
+
+        public Dream OuterDream { get; set; }
+
+        public bool ZeroSleep => InnerDreamTicks == 0 && Depth == 0;
+        public string Description => ZeroSleep ? "none" : $"dream {Depth} - {InnerDreamTicks + Ticks} ticks";
+
+    }
+}
